195ad60eb7ad4c518d12a6986a5584e3b7374517
Andrew TOwnsley <atownsley86@gmail.com>
2022-02-12 18:26:28 -0700

    Initial file setup for Recipe REST API.

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser'
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+// app.use('/recipes', recipeRoutes);
+
+app.get('/', (req, res) => res.send('** Home Page **'))
+
+app.listen(PORT, () => console.log(`Server is Listening on PORT:${PORT}`))
