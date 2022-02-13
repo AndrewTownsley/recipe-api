c51245e7f41763534684b943cbf248bccdcb4565
Andrew TOwnsley <atownsley86@gmail.com>
2022-02-13 15:03:50 -0700

    Error on GET route that returns recipe by name.  recipes.find is not a function ???

---
+++ ReadMe.md
@@ -0,0 +1,19 @@
+*** Walnut Backend API ***
+
+1. Build a GET route that returns all Recipe names.
+
+2. Build a GET route that takes a recipe name as a string param.  Return all the ingredients and the number of steps as JSON.
+
+3. Add a POST route that can add additional recipes  in the existing format to the backend with support for the above routes.
+    # If recipe already exists, return 
+    {
+        error: "Recipe already exists"
+    } 
+    status: 400
+
+4. Adda put route that can update existing recipes.
+    # If recipe does not exist, return 
+    {
+        error: "Recipe does not exist"
+    } 
+    status: 400
--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,52 @@
 import express from 'express';
 import { readFile } from 'fs/promises';
-const jsonData = JSON.parse(
+const recipes = JSON.parse(
     await readFile(
         new URL('../data.json', import.meta.url)
     )
 )
-console.log(jsonData);
 
+
+// Declare router
 const router = express.Router();
 
+// Declare GET route to get all recipes
 router.get('/', (req, res) => {
-    console.log(recipes);
     res.send(recipes)
 })
+
+// GET route to get a recipe by name
+router.get('/:name', (req, res) => {
+    console.log(recipes);
+    const recipe = recipes.find((recipe) => recipe.name === req.params.name)
+    if (recipe) {
+        res.send(recipe)
+    } else {
+        res.status(404).send({ error: 'Recipe not found' })
+    }
+})
+
+
+// POST route to create new recipe
+router.post('/', (req, res) => {
+    const recipe = req.body;
+    recipes.push({...recipe})
+    res.send(`Recipe with name: ${recipe.name} has ben created.`)
+})
+
+// PATCH route to update recipe
+router.patch('/:id', (req, res) => {
+    console.log("update route...");
+    const { id } = req.params;
+    const { name, ingredients, instructions } = req.body;
+    const recipe = recipes.find((recipe) => recipe.name === name);
+
+    if(name) recipe.name = name;
+    if(ingredients) recipe.ingredients = ingredients;
+    if(instructions) recipe.instructions = instructions;
+
+    res.send(`Recipe with name: ${name} has been updated`);
+})
+
 
 export default router;
